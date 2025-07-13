56cd69427b34ad4e18f66161908b1b32c2a5b9ea
Sanskriti Negi <sanskritinegi37@gmail.com>
2025-07-13 12:26:33 +0530

    Redirect root URL to /new by default

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function HomePage() {
-  return <main className="min-h-screen flex items-center justify-center">{/* Blank page */}</main>
+  redirect("/new")
 }
